0b9f992797ffa0ae01a09301d97ac20a9654d3c8
Ciurski <s.bidowaniec@gmail.com>
2020-12-16 17:25:07 +0100

    button styling

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Sidebar} from '../sidebar/sidebar';
 import {Foot} from '../footer/footer';
-import 'antd/dist/antd.css';
 import { Layout, Breadcrumb } from 'antd';
 
 const { Header, Content } = Layout;
@@ -15,11 +14,11 @@ export class Home extends React.Component<any,any> {
           <Header className="site-layout-background" style={{ padding: 0 }} />
           <Content style={{ margin: '0 16px' }}>
             <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+              <Breadcrumb.Item>Competitors</Breadcrumb.Item>
+              <Breadcrumb.Item>Competitor</Breadcrumb.Item>
             </Breadcrumb>
             <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-              Bill is a cat.
+              Results
             </div>
           </Content>
           <Foot />
--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'antd/dist/antd.css';
 import { Layout, Menu } from 'antd';
 import {
   DesktopOutlined,
@@ -8,7 +7,7 @@ import {
   TeamOutlined,
   UserOutlined,
 } from '@ant-design/icons';
-
+import logo from '../img/AK47.svg';
 const { Sider } = Layout;
 const { SubMenu } = Menu;
 
@@ -27,20 +26,20 @@ export class Sidebar extends React.Component<any,any> {
       const { collapsed } = this.state;
       return (
           <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
-            <div className="logo" />
+            <div className="logo"><img src={logo} alt="Logo" /></div>
             <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
               <Menu.Item key="1" icon={<PieChartOutlined />}>
-                Option 1
+                Register
               </Menu.Item>
               <Menu.Item key="2" icon={<DesktopOutlined />}>
-                Option 2
+                Calendar
               </Menu.Item>
-              <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-                <Menu.Item key="3">Tom</Menu.Item>
-                <Menu.Item key="4">Bill</Menu.Item>
-                <Menu.Item key="5">Alex</Menu.Item>
+              <SubMenu key="sub1" icon={<UserOutlined />} title="Competitors">
+                <Menu.Item key="3">Results</Menu.Item>
+                <Menu.Item key="4">Trainings</Menu.Item>
+                <Menu.Item key="5">News</Menu.Item>
               </SubMenu>
-              <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+              <SubMenu key="sub2" icon={<TeamOutlined />} title="Contests">
                 <Menu.Item key="6">Team 1</Menu.Item>
                 <Menu.Item key="8">Team 2</Menu.Item>
               </SubMenu>
